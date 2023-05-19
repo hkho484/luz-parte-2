d30b9bcbd9f1e5ebfd00146415524548685bd043
hkho484 <133361308+hkho484@users.noreply.github.com>
2023-05-19 13:22:58 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.lightLevel() < 65) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
